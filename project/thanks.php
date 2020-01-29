dc17cbcb3ae8737fa32b4c87af37a71b38542914
SAlexVL <salexandervl@gmail.com>
2020-01-29 18:41:07 +0300

    Сделан весь проект с нуля

---
+++ project/thanks.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/logo.png" >
+    <link rel="stylesheet" href="css/thanks.css">
+    <title>Ремонт квартир</title>
+</head>
+<body>
+    <nav class="nav">
+        <div class="container">
+            <div class="nav-block">
+                <div class="nav-block__logo wow flip" data-wow-delay="0.1s">
+                    <a href="index.html">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <!-- /.navbar-block__logo -->
+                <div class="nav-block__info">
+                    <div class="nav-block__contacts">
+                        <span class="nav-block__address">Калуга, Москва, МО</span>
+                        <a href="tel:+7(495)42-251-31" class="nav-block__phone">+7 (495) 42-251-31</a>
+                    </div>
+                    <!-- /.navbar__contacts -->
+                </div>
+                <!-- /.navbar_info -->
+            </div>
+            <!-- /.navbar-block -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- /.navbar -->
+    <main>
+        <section class="thank">
+            <div class="container">
+                <div class="thank-block">
+                    <div class="thank-text">
+                        <h1 class="thank-text__title">
+                            Спасибо за заявку 
+                        </h1>
+                        <span class="thank-text__subtitle">
+                            Мы перезвоним через 15 минут
+                        </span>
+                        <div class="btn">
+                            <a href="index.php" class="button thank-block__button" id="button">Вернуться на главную страницу</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.hero-block -->
+            </div>
+            <!-- /.container -->
+        </section>
+        <!-- /.hero -->
+    </main>
+
+
+    <!-- <script src="js/main.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="js/modal.js"></script>
+    <script src="js/button-back.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/slider-slick.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/jquery.maskedinput.min.js"></script>
+    <script src="js/validate.js"></script>
+    
+    <script>
+        /* Инициализация WOW.js */
+        new WOW().init();
+    </script>
+</body>
+</html>
